497f121afd5cf92856089742624c650f7289873b
toqasaad97 <toqasaad45@gmail.com>
2024-08-15 00:08:15 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx';
+import '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free';
 import './index.css'
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  // base: '/task-react/',
-
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/fontawesome-free/css/all.min.css']
+    }
+  }
+});
